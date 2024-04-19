6eccea852c8bd786d0bd930682fb40a534e96bcd
abdulhakim-altunkaya <altunkaya2@gmail.com>
2024-04-19 22:52:08 +0300

    Version 2: contract moved to readme

--- ignition/modules/Staking.js
+++
@@ -1,8 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("StakingModule", (m) => {
-
-  const stakingContract = m.contract("Staking");
-
-  return { stakingContract };
-});
