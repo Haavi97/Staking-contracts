1a608db19c0824ba0e51d9161e4a31c7e1ce73f2
abdulhakim-altunkaya <altunkaya2@gmail.com>
2024-04-20 18:32:35 +0300

    version 5: vesting contract updated

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -5,12 +5,13 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Vesting is Ownable {
-    event TokensVested(address indexed worker, uint256 tokens);
+    event WorkerAdded(address indexed worker);
+    event RewardWithdrawn(address indexed worker, uint256 amount);
+
     //***********STATE VARIABLES*************
     struct Worker {
         uint256 startTime; // The time when the worker starts earning tokens
-        bool hasWithdrawn;
-        uint256 finalBalance;
+        uint256 tokensEarned;
     }
 
     mapping(address => Worker) public workersMapping; // Maps worker addresses to their details
@@ -22,21 +23,22 @@ contract Vesting is Ownable {
     //apy "1" means %0,1
     //apy "10" means %1
     //apy "100" means %10
-    uint public swapRate; //Swap rate for usdt.
+    uint public swapRate; //Swap rate for usdt. 
     //swapRate "1" means 10 company token is equal to 1 usdt token
-    //swapRate "10" means 10 company token is equal to 10 usdt token
-    //swapRate "100" means 10 company token is equal to 100 usdt token
+    //swapRate "10" means 10 company token is equal to 10 usdt token 
+    //swapRate "100" means 10 company token is equal to 100 usdt token 
     //Calculations are done
     uint public timePeriod; //number of months that needs to pass before company profit reporting. Like 6, 12
     //this will be used to prevent workers to claim final rewards before vesting period ends
-    bool public isClaimOpen = false;
+    bool public isPaused = false;
+
 
     //*********CONSTRUCTOR*************
     constructor(
-        address _tokenAddress,
-        uint _baseReward,
-        uint _apy,
-        uint _swapRate,
+        address _tokenAddress, 
+        uint _baseReward, 
+        uint _apy, 
+        uint _swapRate, 
         uint _timePeriod
     ) {
         tokenA = IERC20(_tokenAddress);
@@ -50,8 +52,8 @@ contract Vesting is Ownable {
     error NotWorker(address caller, string message);
     modifier onlyWorkers() {
         if (workersMapping[msg.sender].startTime == 0) {
-            revert NotWorker(msg.sender, "you have 0 reward");
-        }
+            revert NotWorker(msg.sender, "you have 0 rewards");
+        } 
         _;
     }
 
@@ -60,16 +62,22 @@ contract Vesting is Ownable {
     // Function to add a worker and start vesting
     function addWorker(address _worker) external onlyOwner {
         require(
-            workersMapping[_worker].startTime == 0,
+            workersMapping[_worker].startTime == 0, 
             "Worker already exists"
         );
-
-        workersMapping[_worker] = Worker(block.timestamp, false, 0);
+        workersMapping[_worker] = Worker(block.timestamp, 0);
         workersArray.push(_worker);
+        emit WorkerAdded(_worker);
     }
+
+
     // Function to set token token address dynamically
     function setToken(address _tokenAddress) external onlyOwner {
         tokenA = IERC20(_tokenAddress);
+    }
+
+    function togglePause() external onlyOwner {
+        isPaused = !isPaused;
     }
 
     //Company will provide liquidity to the contract so that we can distribute rewards later.
@@ -83,80 +91,49 @@ contract Vesting is Ownable {
         tokenA.transferFrom(msg.sender, address(this), amount);
     }
 
-    //workers can see how much they will earn if they stick to the end of vesting period
-    uint public unbrokenAmount;
-    //Company will use this function to calculate unbrokenAmount and later workers who did
-    //not withdraw will be rewarded this exact amount. It will save finalizeVesting function
-    //from making unnecessary computations
-    function calculateUnbroken() external onlyOwner {
-        uint balance;
-        for (uint256 index = 1; index < timePeriod + 1; index++) {
-            balance = balance + (baseReward + (((balance) * apy) / 1000));
-        }
-        unbrokenAmount = balance;
+    //People can see how much will earn by entering any number of months
+    function calculateReward(uint _numberMonths) external view returns(uint) {
+        require(_numberMonths < 100 && _numberMonths > 0, "please enter a valid number");
+        uint rewardAmount = (((apy / 1000) * _numberMonths) + baseReward) * _numberMonths;
+        return rewardAmount;
     }
 
-    //**********MAIN FUNCTIONS************
-    function finalizeVesting() external onlyOwner {
-        for (uint256 i = 0; i < workersArray.length; i++) {
-            address targetWorker = workersArray[i];
-
-            // Workers who didn not withdraw, we know their total reward+apy
-            // For workers who withdraw, we need to calculate as below
-            if (workersMapping[targetWorker].hasWithdrawn == true) {
-                //First find how many months passed since last withdraw
-                uint rewardStart = workersMapping[targetWorker].startTime;
-                uint monthsPassed = (block.timestamp - rewardStart) / (30 days);
-                //Then we create balance variable to store total reward+apy
-                uint balance;
-                for (uint256 index = 1; index < monthsPassed + 1; index++) {
-                    balance =
-                        balance +
-                        (baseReward + (((balance) * apy) / 1000));
-                }
-                workersMapping[targetWorker].finalBalance = balance;
-            } else {
-                workersMapping[targetWorker].finalBalance = unbrokenAmount;
-            }
-        }
-        isClaimOpen = true;
-    }
-
-    // Function to declare profits and reward workers proportionally
-    function claimReward() external onlyWorkers {
-        require(isClaimOpen, "Claiming is not open yet");
-        uint rewardAmount = workersMapping[msg.sender].finalBalance;
-        require(rewardAmount > 0, "You have already withdrawn final reward");
-        workersMapping[msg.sender].finalBalance = 0;
-        //The weird calculation here is to accommodate decimal swap rates such as %0.1
-        uint amount = (rewardAmount / (10 / swapRate)) * (10 ** 18);
-        tokenA.transfer(msg.sender, amount);
-    }
-
+    //**********MAIN FUNCTION************
     //Workers will be able to withdraw tokens before company profit reporting
-    //decimals handled inside the function.
-    function withdrawTokens() external onlyWorkers {
+    //decimals handled inside the function. 
+    function withdrawTokens() external onlyWorkers  {
+        require(isPaused == false, "Contract is paused");
         //first we calculate how many months passed
         uint rewardStart = workersMapping[msg.sender].startTime;
         uint monthsPassed = (block.timestamp - rewardStart) / (30 days);
-        uint reward = (apy*baseReward*monthsPassed + baseReward)*monthsPassed;
-        //Then we create balance variable to store total reward+apy
-        uint balance;
-        for (uint256 index = 1; index < monthsPassed + 1; index++) {
-            balance = balance + (baseReward + (((balance) * apy) / 1000));
+        if(monthsPassed > timePeriod ) {
+            monthsPassed = timePeriod;
         }
+        //Divide apy by 1000 to accommodate apy under 10. Then multiply with monthsPassed. Then add baseReward
+        //Then multiply with months passed
+        uint rewardAmount = (((apy / 1000) * monthsPassed) + baseReward) * monthsPassed;
+
         workersMapping[msg.sender].startTime = block.timestamp;
-        workersMapping[msg.sender].hasWithdrawn = true;
+        workersMapping[msg.sender].tokensEarned += rewardAmount;
         //The weird calculation here is to accommodate decimal swap rates such as %0.1
-        uint amount = (balance / (10 / swapRate)) * (10 ** 18);
+        uint amount = rewardAmount/(10/swapRate)*(10**18);
         tokenA.transfer(msg.sender, amount);
+        emit RewardWithdrawn(msg.sender, rewardAmount);
     }
 
-    //MORE FUNCTIONS:
-    //Resetting mapping after each profit declation by the company
-    //Security checks
-    //more view functions
-    //Integrate vestTokens function to declareProfits with a forLoop
     //add a pauseStatus security check to the contract
     //withdraw tokens will be called by user account or else?
+
+    function displayRewards() external view returns(uint) onlyWorkers  {
+        uint rewardStart = workersMapping[msg.sender].startTime;
+        uint monthsPassed = (block.timestamp - rewardStart) / (30 days);
+        if(monthsPassed > timePeriod ) {
+            monthsPassed = timePeriod;
+        }
+        //Divide apy by 1000 to accommodate apy under 10. Then multiply with monthsPassed. Then add baseReward
+        //Then multiply with months passed
+        uint rewardAmount = (((apy / 1000) * monthsPassed) + baseReward) * monthsPassed;
+        return rewardAmount;
+    }
+
 }
